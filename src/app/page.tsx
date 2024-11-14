7e207ae5bce5cd8e08e6f6e26545f7010b4d7540
fengjin <fengjin@baiwang.com>
2024-11-14 13:59:37 +0800

    上传样式完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import Home from '@/components/Home';
 
 import UpLoad from '@/components/UpLoad';
-// import UpLoad2 from '@/components/UpLoad2';
+import UpLoad2 from '@/components/UpLoad2';
 
 export default async function Page() {
   return (
     <>
-      {/* <div style={{ padding: '100px' }}>
+      <div style={{ padding: '100px' }}>
         <UpLoad2 />
-      </div> */}
+      </div>
 
       <div style={{ padding: '100px' }}>
         <UpLoad />
--- src/components/UpLoad2/index.tsx
+++ src/components/UpLoad2/index.tsx
@@ -6,59 +6,58 @@ import cls from 'classnames';
 
 import styles from './index.module.scss';
 
-interface Props extends PropsBase {}
+interface Props extends PropsBase {
+  sizeWidth?: string;
+  zoom?: number;
+  fontSize?: string;
+}
 
 const UpLoad = (props: Props) => {
-  const {} = props;
+  const { sizeWidth = '80px', zoom = 0.8, fontSize = '15px' } = props;
 
   useEffect(() => {});
 
-  const size = 40;
-
-  const style: any = { '--size': `${size}px` };
+  const style: any = {
+    '--size': `${zoom * 160}px`,
+    '--size-width': sizeWidth,
+    '--font-size': fontSize,
+  };
 
   return (
     <>
-      <div className={styles.upload} id='upload' style={style}>
-        <svg width='100' height='100' viewBox='0 0 100 100'>
-          <defs>
-            <clipPath id='clipIt'>
-              <circle fill='black' r='35' cy='50' cx='50' />
-            </clipPath>
-          </defs>
-          <path
-            className={styles.utveckling}
-            d='M 49.99955,21.646452 A 28.35355,28.353548 0 0 0 21.646,50
-                    28.35355,28.353548 0 0 0 49.99955,78.353548
-                    28.35355,28.353548 0
-                    0 0 78.3531,50 28.35355,28.353548 0 0 0 49.99955,21.646452
-                    Z'
-          />
-          <g className={styles.cut} clip-path='url(#clipIt)'>
-            <circle r='35' cy='50' cx='50' className={styles.p1} />
-            <g className={styles.pil}>
+      <div className={styles.container} style={style}>
+        <label className={styles.label}>
+          <input type='checkbox' className={styles.input} />
+          {/* <input
+            type='file'
+            id='file'
+            name='file'
+            multiple
+            className={styles.file}
+            style={{ position: 'absolute', clip: 'rect(0 0 0 0)' }}
+          /> */}
+          <span className={styles.circle}>
+            <svg
+              className={styles.icon}
+              aria-hidden='true'
+              fill='none'
+              viewBox='0 0 24 24'>
               <path
-                className={styles.p2}
-                d='m 40.056531,47.971893 9.914191,-9.514425
-                                    9.994144,9.514425'
-              />
-              <path
-                className={styles.p3}
-                d='M 49.970722,38.457468 V 61.56393'
-              />
-            </g>
-            <path
-              className={styles.bock}
-              d='m 49.533063,13 c 0,0
-                                        -4.038975,0.243204 -7.084664,1.6
-                                        -10.965877,4.88509 -12.03936,12.900449
-                                        -12.029016,15.995361 0.02524,7.552595
-                                        4.607455,12.168562 6.129905,13.956538 C
-                                        41.41924,50.271203 47.447623,56.491
-                                        47.447623,56.491 L 63.320719,42.911532'
-            />
-          </g>
-        </svg>
+                stroke='currentColor'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='1.5'
+                d='M12 19V5m0 14-4-4m4 4 4-4'></path>
+            </svg>
+            <div className={styles.square}></div>
+          </span>
+          <div className={cls(styles.title, styles.text1)}>
+            <span>上传图片</span>
+          </div>
+          <div className={cls(styles.title, styles.text2)}>
+            <span>上传完成</span>
+          </div>
+        </label>
       </div>
     </>
   );
